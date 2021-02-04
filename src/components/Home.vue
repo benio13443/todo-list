be5b7b70ea2e15e1da59e6ef2ade88526cd1c236
Benio Katauchi <ruby13443@gmail.com>
2021-02-04 20:16:19 +0900

    Initial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="home_container">
+        <section v-for="item in sortedItemsByAmount" v-bind:key="item.id">
+            <p class="todo_contents">
+                {{ item.title }}{{ item.deadLine }}{{ item.expectedTime }}
+            </p>
+        </section>
+    </div>
+</template>

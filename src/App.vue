7fe14238b8d3e3c40fe3d03dcb143ab76595ccbd
Benio Katauchi <ruby13443@gmail.com>
2021-02-11 18:44:36 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,6 @@
   <div id="app">
     <Header></Header>
     <Addition></Addition>
-    <!-- <div class="items-part">
-      <form v-on:submit.prevent>
-        <input type="text" v-model="newItem.title">
-        <label>締め切り時間:
-          <input type="date" value="00:00" v-model="newItem.deadLine">
-        </label>
-        <label>見積り時間:
-          <input type="time" value="00:00" v-model="newItem.expectedTime">
-        </label>
-        <button v-on:click="addItem">create</button>
-        <!-- value属性値は、初期入力値となる。
-        時間入力欄のvalue属性値は、hh:mm:ssの形式で入力する -->
-      <!-- </form>
-      <ul>
-        <li v-for="(item, index) in items" v-bind:key="index">{{ item.title }}{{ item.deadLine }}{{ item.expectedTime }}
-          <div v-on:click="deleteItem(index)" class="delete">
-          </div>
-        </li>
-        <pre> {{ $data }} </pre>
-      </ul>
-    </div> --> -->
     <Home></Home>
     <!-- <div class="add-icon">
       <img src="add_icon.png" class="addIcon">
@@ -47,7 +26,7 @@
           title: "",
           deadLine: "",
           expectedTime: "",
-          priority:"",
+          priority: "",
         },
         items: [],
       }
@@ -98,4 +77,5 @@
       }
     },
   };
+
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <section class="todayDate">
-      <!-- <p>{{setToday}}</p> -->
       <h1>2020年12月17日（木）</h1>
     </section>
   </div>
